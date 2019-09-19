20472e9cf539c792816cec4f8ee7e0a5d136a750
Rajashree <chiragjadav134@gmail.com>
2019-09-20 00:05:50 +0530

    Flutter Music App

---
+++ build/frontend_server.d
@@ -0,0 +1,1 @@
+frontend_server.d: /Users/chiragjadav/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
---
+++ lib/presenter_screen.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+class PresenterScreen extends StatefulWidget {
+  PresenterScreen({Key key}) : super(key: key);
+
+  @override
+  _PresenterScreenState createState() {
+    return _PresenterScreenState();
+  }
+}
+
+class _PresenterScreenState extends State<PresenterScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: Scaffold(
+        body: new Column(
+          children: <Widget>[
+
+          ],
+        ),
+      ),
+    );
+  }
+}
